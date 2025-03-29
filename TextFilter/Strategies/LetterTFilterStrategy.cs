7be87e139316820afb286c1dfdea93e93502128a
Lois Martins <Lois.Martins@hotmail.com>
2025-03-29 22:11:45 +0000

    Remove unused namespaces

--- TextFilter/Strategies/LetterTFilterStrategy.cs
+++ TextFilter/Strategies/LetterTFilterStrategy.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TextFilter.Strategies
+﻿namespace TextFilter.Strategies
 {
     public class LetterTFilterStrategy : IFilterStrategy
     {
